349996dd817833348e73816af478aca9fbc96172
Cody's Main Laptop <codster761@gmail.com>
2020-08-25 20:19:27 +1000

    Implemented attack combos and damageable entities

---
+++ Done in the Fungeon/Assets/Scripts/Damager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter2D(Collision2D c) { 
+		if(c.gameObject.GetComponent<Damageable>() != null) {
+			Debug.Log(c.gameObject.name);
+		}
+		Debug.Log("Anus");
+	}
+}
--- Done in the Fungeon/Assets/Scripts/Player.cs
+++ Done in the Fungeon/Assets/Scripts/Player.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 1f;
+	public float attackDuration;
+	public float comboTime;
+	
+	private float attackDurationTimer;
+	private float comboTimer;
+	
+	public GameObject damageZone1;
+	public GameObject damageZone2;
+	public GameObject damageZone3;
+	public GameObject damageZone4;
+	public GameObject damageZone5;
+	
+	//States
+	enum State
+	{
+		Default,
+		Combo1,
+		Combo2,
+		Combo3,
+		Combo4,
+		Combo5,
+		Dash,
+		ChargedAttack
+	};
+	private State state;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +40,164 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		switch(state) {
+			case State.Default:
+				if(Input.GetButtonDown("Attack"))
+				{
+					comboTimer = comboTime;
+					attackDurationTimer = attackDuration;
+					state = State.Combo1;
+				}
+				break;
+			case State.Combo1:
+				comboTimer -= Time.deltaTime;
+				
+				// Attack Duration
+				if(attackDurationTimer >= 0)
+				{
+					attackDurationTimer -= Time.deltaTime;
+					damageZone1.gameObject.SetActive(true);
+				}
+				else
+				{
+					damageZone1.gameObject.SetActive(false);
+				}
+				
+				if(comboTimer < 0)
+				{
+					state = State.Default;
+				}
+				
+				if(Input.GetButtonDown("Attack"))
+				{
+					comboTimer += comboTime;
+					attackDurationTimer = attackDuration;
+					state = State.Combo2;
+				}
+				break;
+			case State.Combo2:
+				comboTimer -= Time.deltaTime;
+				
+				// Attack Duration
+				if(attackDurationTimer >= 0)
+				{
+					attackDurationTimer -= Time.deltaTime;
+					damageZone2.gameObject.SetActive(true);
+				}
+				else
+				{
+					damageZone2.gameObject.SetActive(false);
+				}
+				
+				if(comboTimer < 0)
+				{
+					state = State.Default;
+				}
+				
+				if(Input.GetButtonDown("Attack"))
+				{
+					comboTimer += comboTime;
+					attackDurationTimer = attackDuration;
+					state = State.Combo3;
+				}
+				break;
+			case State.Combo3:
+				comboTimer -= Time.deltaTime;
+				
+				// Attack Duration
+				if(attackDurationTimer >= 0)
+				{
+					attackDurationTimer -= Time.deltaTime;
+					damageZone3.gameObject.SetActive(true);
+				}
+				else
+				{
+					damageZone3.gameObject.SetActive(false);
+				}
+				
+				if(comboTimer < 0)
+				{
+					state = State.Default;
+				}
+				
+				if(Input.GetButtonDown("Attack"))
+				{
+					comboTimer += comboTime;
+					attackDurationTimer = attackDuration;
+					state = State.Combo4;
+				}
+				break;
+			case State.Combo4:
+				comboTimer -= Time.deltaTime;
+				
+				// Attack Duration
+				if(attackDurationTimer >= 0)
+				{
+					attackDurationTimer -= Time.deltaTime;
+					damageZone4.gameObject.SetActive(true);
+				}
+				else
+				{
+					damageZone4.gameObject.SetActive(false);
+				}
+				
+				if(comboTimer < 0)
+				{
+					state = State.Default;
+				}
+				
+				if(Input.GetButtonDown("Attack"))
+				{
+					comboTimer += comboTime;
+					attackDurationTimer = attackDuration;
+					state = State.Combo5;
+				}
+				break;
+			case State.Combo5:
+				comboTimer -= Time.deltaTime;
+				
+				// Attack Duration
+				if(attackDurationTimer >= 0)
+				{
+					attackDurationTimer -= Time.deltaTime;
+					damageZone5.gameObject.SetActive(true);
+				}
+				else
+				{
+					damageZone5.gameObject.SetActive(false);
+				}
+				
+				if(comboTimer < 0)
+				{
+					state = State.Default;
+				}
+				break;
+			case State.Dash:
+				break;
+			case State.ChargedAttack:
+				break;
+		}
+		
         float velocity = moveSpeed * Time.deltaTime;
 
         float verticalMove = Input.GetAxis("Vertical") * velocity;
         float horizontalMove = Input.GetAxis("Horizontal") * velocity;
 
         transform.Translate(horizontalMove, verticalMove, 0);
+		
+		/*
+		if(Input.GetButtonDown("Attack"))
+		{
+			attackDurationTimer += attackDuration;
+		}
+		
+		if(attackDurationTimer >= 0) {
+			damageZone.gameObject.SetActive(true);
+			attackDurationTimer -= Time.deltaTime;
+		} else {
+			damageZone.gameObject.SetActive(false);
+		}
+		*/
 
 /*        if (Input.GetButton("up"))
         {

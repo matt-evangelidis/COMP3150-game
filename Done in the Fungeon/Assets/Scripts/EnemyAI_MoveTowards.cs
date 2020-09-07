9882aa6ba5f54e9f911f86fca564cc87ffcd9f9d
Cody's Main Laptop <codster761@gmail.com>
2020-09-08 00:01:11 +1000

    Added a graph to the game for pathfinding (still not complete)

---
+++ Done in the Fungeon/Assets/Scripts/EnemyAI_MoveTowards.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_MoveTowards : MonoBehaviour
+{
+	/*public GenerateNav nav;
+	private List<List<Node>> grid;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		grid = nav.getGrid();
+        for(int i = 0;i<grid.Count;i++)
+		{
+			for(int j = 0;j<grid[i].Count;j++)
+			{
+				for(int k = 0;k<grid[i][j].neighbours.Count;k++)
+				{
+					Debug.Log("(" + i + ", " + j + ") " + grid[i][j].neighbours[k].position);
+				}
+			}
+		}
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Done in the Fungeon/Assets/Scripts/GenerateNav.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNav : MonoBehaviour
+{
+	public struct Node
+	{
+		public Transform position;
+		public List<Node> neighbours;
+	}
+	private List<List<Node>> grid;
+	private const int gridSize = 13;
+	
+	void Awake()
+	{
+		// basically get all of the children into a list of list of structs
+		grid = new List<List<Node>>();
+		for(int i = 0;i<transform.childCount;i++)
+		{
+			List<Node> tempList = new List<Node>();
+			for(int j = 0;j<transform.GetChild(i).transform.childCount;j++)
+			{
+				Node n = new Node();
+				n.position = transform.GetChild(i).GetChild(j);
+				n.neighbours = new List<Node>();
+				tempList.Add(n);
+			}
+			grid.Add(tempList);
+		}
+		
+		// generate neighbour lists
+		for(int i = 0;i<grid.Count;i++)
+		{
+			for(int j = 0;j<grid[i].Count;j++)
+			{
+				List<Node> neighbourList = new List<Node>();
+				
+				// neighbours added in the order: above, right, below, left
+				if(i-1 >= 0 && grid[i-1][j].position.gameObject.tag == "nav_enable")
+				{
+					grid[i][j].neighbours.Add(grid[i-1][j]);
+				}
+				
+				if(j-1 >= 0 && grid[i][j-1].position.gameObject.tag == "nav_enable")
+				{
+					grid[i][j].neighbours.Add(grid[i][j-1]);
+				}
+				
+				if(i+1 <= gridSize && grid[i+1][j].position.gameObject.tag == "nav_enable")
+				{
+					grid[i][j].neighbours.Add(grid[i+1][j]);
+				}
+				
+				if(j+1 <= gridSize && grid[i][j+1].position.gameObject.tag == "nav_enable")
+				{
+					grid[i][j].neighbours.Add(grid[i][j+1]);
+				}
+				
+				//Debug.Log("(" + i + ", " + j + ") " + grid[i][j].position);
+			}
+		}
+		
+		/*for(int i = 0;i<grid.Count;i++)
+		{
+			for(int j = 0;j<grid[i].Count;j++)
+			{
+				for(int k = 0;k<grid[i][j].neighbours.Count;k++)
+				{
+					Debug.Log("(" + i + ", " + j + ") " + grid[i][j].neighbours[k].position);
+				}
+			}
+		}*/
+	}
+	
+	public List<List<Node>> getGrid()
+	{
+		return grid;
+	}
+}

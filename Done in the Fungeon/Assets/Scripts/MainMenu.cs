9d448ef2096319707b63bb307abe41e690a9401f
mqlnx <lin.xie1@students.mq.edu.au>
2020-09-10 15:27:11 +1000

    Added main menu C# script. Start game goes to the test room scene for now, quit game only works on build. Added test and laser room to the build open scenes. Still implementing settings panel.

---
+++ Done in the Fungeon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Test Room");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

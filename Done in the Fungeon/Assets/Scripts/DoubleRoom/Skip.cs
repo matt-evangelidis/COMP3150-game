a1c856ab5e92756975a1b8d9f79ddc8774059994
Cody's Main Laptop <codster761@gmail.com>
2020-10-29 01:15:52 +1100

    Added reset and skip features to certain rooms

---
+++ Done in the Fungeon/Assets/Scripts/DoubleRoom/Skip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skip : MonoBehaviour
+{
+	public GameObject button0;
+	public GameObject button1;
+	public GameObject button2;
+	public GameObject button3;
+	public GameObject slower0;
+	public GameObject slower1;
+	public GameObject clone0;
+	public GameObject clone1;
+	public GameObject clone2;
+	public GameObject exitDoor;
+	public GameObject walls;
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("r") && Input.GetKey("right shift"))
+		{
+			button0.SetActive(false);
+			button1.SetActive(false);
+			button2.SetActive(false);
+			button3.SetActive(false);
+			slower0.SetActive(false);
+			slower1.SetActive(false);
+			clone0.SetActive(false);
+			clone1.SetActive(false);
+			clone2.SetActive(false);
+			exitDoor.SetActive(false);
+			walls.SetActive(false);
+		}
+    }
+}
---
+++ Done in the Fungeon/Assets/Scripts/Golf Room/ResetKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetKey : MonoBehaviour
+{
+	public BallReset resetter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("r"))
+		{
+			resetter.Reset();
+		}
+    }
+}
